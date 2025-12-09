350f63ba7095716087fda530711000c73c5d24c7
Colin <colinleonghongseng@gmail.com>
2025-12-09 15:10:08 +0800

    test simple scotty

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import qualified Web.Scotty
+
+greet :: Web.Scotty.ActionM ()
+greet = Web.Scotty.html "<h1>Hello, adam</h1>"
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    Web.Scotty.scotty 3000 $ do
+        Web.Scotty.get "/" $ do
+            greet
