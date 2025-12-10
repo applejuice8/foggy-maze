2eb357963d939bf48301e894ab894beef48e7084
Colin <colinleonghongseng@gmail.com>
2025-12-10 11:27:09 +0800

    Separated create tables into a separate function

--- app/Models.hs
+++ app/Models.hs
@@ -6,15 +6,8 @@ import Models (Stock)
 connectDB :: IO Connection
 connectDB = open "stocks.db"
 
-insertStock :: Connection -> Stock -> IO ()
-insertStock conn stock =
-    execute_ conn "INSERT INTO stock (code, name) VALUES (?, ?)" stock
-
-main :: IO ()
-main = do
-    conn <- connectDB
-
-    -- Create tables
+createTables :: Connection -> IO ()
+createTables conn =
     execute_ conn "CREATE TABLE IF NOT EXISTS stock (\
                     \code TEXT PRIMARY KEY,\
                     \name TEXT\
@@ -27,7 +20,18 @@ main = do
                     \tax REAL,\
                     \FOREIGN KEY(stock) REFERENCES stock(code)\
                 \)"
-    
+
+insertStock :: Connection -> Stock -> IO ()
+insertStock conn stock =
+    execute_ conn "INSERT INTO stock (code, name) VALUES (?, ?)" stock
+
+main :: IO ()
+main = do
+    conn <- connectDB
+
+    -- Create tables
+    createTables conn
+
     -- Insert stock
     insertStock conn (Stock "AAPL" "Apple")
 
