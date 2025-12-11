c5bf4856570e4a2c93f6a230ccf022c0f054033c
Colin <colinleonghongseng@gmail.com>
2025-12-11 18:39:21 +0800

    Completed recursive index getter

--- app/Main.hs
+++ app/Main.hs
@@ -18,8 +18,30 @@ initialMaze =
     , "#########"
     ]
 
+-- putStrLn for each list item
 printMaze :: Maze -> IO ()
 printMaze = mapM_ print
+
+-- Find column of a character in a row recursively
+getColIndex :: Char -> String -> Int -> Maybe Int
+getColIndex _ [] _ = Nothing
+getColIndex symbol (col:cols) x
+    | col == symbol = Just x
+    | otherwise = getColIndex symbol cols (x + 1)
+
+-- Find the (row, col) in the 2D list, recursively
+getRowIndex :: Char -> [String] -> Int -> (Int, Int)
+getRowIndex _ [] _ = error "Symbol not found"
+getRowIndex symbol (row:rows) y =
+    case getColIndex symbol row 0 of
+        Just col -> (y, col)
+        Nothing  -> getRowIndex symbol rows (y + 1)
+
+
+handleKey :: Char -> IO ()
+handleKey c = 
+    case c of
+        'W' -> 
 
 main :: IO ()
 main = do
@@ -32,6 +54,7 @@ main = do
 loop :: Maze -> IO ()
 loop maze = do
     c <- getChar
+    handleKey c
     putStrLn $ "You pressed: " ++ [c]
     printMaze maze
     if c == 'q'
