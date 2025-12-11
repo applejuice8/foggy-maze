71791293307e6aea5770636df5c75a1928ad5f46
Colin <colinleonghongseng@gmail.com>
2025-12-11 14:22:58 +0800

    Start afresh with new topic

--- app/DataTypes.hs
+++ app/DataTypes.hs
@@ -1,16 +1,2 @@
 module DataTypes where
 
-data Stock = Stock
-    { stockCode :: String,
-    , stockName :: String
-    } deriving Show
-
-data BuySell = Buy | Sell
-    deriving (Show, Eq)
-
-data Transaction = Transaction
-    { stockCode :: String
-    , type :: BuySell
-    , price :: Double
-    , tax :: Double
-    } deriving Show
