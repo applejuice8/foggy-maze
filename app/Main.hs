ad9d7225eed9c1af7a2c85c8b7fb1a168bde6404
Colin <colinleonghongseng@gmail.com>
2025-12-11 14:37:35 +0800

    Test movement keys

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,16 @@
-{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import Web.Scotty ( get, html, scotty, ActionM )
-import Data.Text.Lazy (Text)
-
-greet :: Text
-greet = "<h1>Hello, adam</h1>"
+import System.IO
 
 main :: IO ()
-main = scotty 3000 $ do
-    get "/" $ html greet
+main = do
+    hSetBuffering stdin NoBuffering   -- read input immediately
+    hSetEcho stdin False              -- optional: do not display typed keys
+    loop
+
+loop :: IO ()
+loop = do
+    c <- getChar                     -- reads a single key immediately
+    putStrLn $ "You pressed: " ++ [c]
+    if c /= 'q' then loop else return ()
